2fa5944c2d0014d91d9d007f16fa777a2aec32c7
agiselliah <57532018+agiselliah@users.noreply.github.com>
2020-09-14 21:14:08 +0200

    Initial commit

---
+++ no/oslomet/cs/algdat/Oblig1/Oblig1.java
@@ -0,0 +1,95 @@
+package no.oslomet.cs.algdat.Oblig1;
+
+////// Løsningsforslag Oblig 1 ////////////////////////
+
+import java.lang.UnsupportedOperationException;
+
+public class Oblig1 {
+    private Oblig1() {}
+
+    ///// Oppgave 1 //////////////////////////////////////
+    public static int maks(int[] a) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static int ombyttinger(int[] a) {
+        throw new UnsupportedOperationException();
+    }
+
+    ///// Oppgave 2 //////////////////////////////////////
+    public static int antallUlikeSortert(int[] a){
+        for (int i = 0; i < a.length; ++i) {
+            if (a[i] > a[i + 1]) {
+                throw new IllegalStateException("Tabellen er usortert");
+            }
+        }
+
+
+        //deklarer en variabel som teller antall ulike tall den skal være lik 0.
+        int tall = 0;
+
+        //Nå teller vi om hvor mange er ulike i listen:
+
+
+        for (int i = 0; i < a.length; ++i) {
+            while (i < a.length && a[i] == a[i + 1]) {
+                i++;
+            }
+            tall++;
+        }
+        return tall;
+    }
+
+
+
+    ///// Oppgave 3 //////////////////////////////////////
+    public static int antallUlikeUsortert(int[] a) {
+        throw new UnsupportedOperationException();
+    }
+
+    ///// Oppgave 4 //////////////////////////////////////
+    public static void delsortering(int[] a) {
+        throw new UnsupportedOperationException();
+    }
+
+    ///// Oppgave 5 //////////////////////////////////////
+    public static void rotasjon(char[] a) {
+        throw new UnsupportedOperationException();
+    }
+
+    ///// Oppgave 6 //////////////////////////////////////
+    public static void rotasjon(char[] a, int k) {
+        throw new UnsupportedOperationException();
+    }
+
+    ///// Oppgave 7 //////////////////////////////////////
+    /// 7a)
+    public static String flett(String s, String t) {
+        throw new UnsupportedOperationException();
+    }
+
+    /// 7b)
+    public static String flett(String... s) {
+        throw new UnsupportedOperationException();
+    }
+
+    ///// Oppgave 8 //////////////////////////////////////
+    public static int[] indekssortering(int[] a) {
+        throw new UnsupportedOperationException();
+    }
+
+    ///// Oppgave 9 //////////////////////////////////////
+    public static int[] tredjeMin(int[] a) {
+        throw new UnsupportedOperationException();
+    }
+
+    ///// Oppgave 10 //////////////////////////////////////
+    public static int bokstavNr(char bokstav) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static boolean inneholdt(String a, String b) {
+        throw new UnsupportedOperationException();
+    }
+
+}  // Oblig1
